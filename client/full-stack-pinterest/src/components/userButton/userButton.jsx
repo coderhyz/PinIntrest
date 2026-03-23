df8ae0da8ad95973a902f75c40e6449c6ca3cd35
coderhyz <coderhyz@gmail.com>
2026-03-23 22:02:41 +0800

    feat:左边栏和上栏布局完成

--- client/full-stack-pinterest/src/components/userButton/userButton.jsx
+++ client/full-stack-pinterest/src/components/userButton/userButton.jsx
@@ -1,10 +1,27 @@
 import './userButton.css'
+import { useState } from 'react';
 function UserButton() {
-    return (
+    const [open, setOpen] = useState(false);
+    const [currentUser, setCurrentUser] = useState(true)
+    return (currentUser ?
         <div className="userButton">
             <img src="/general/noAvatar.png" alt="User Avatar" />
-            <img className="arrow" src="/general/arrow.svg" alt="Arrow" />
+            <img onClick={() => setOpen(prev => !prev)} className="arrow" src="/general/arrow.svg" alt="Arrow" />
+            {
+                open && <div className="userOptions">
+                    <div className="userOption">Profile</div>
+                    <div className="userOption">Setting</div>
+                    <div className="userOption">Logout</div>
+                </div>
+            }
         </div>
+
+        : (
+            <p className='loginLink'>
+                Login/SingUP
+            </p>
+        )
+
     );
 }
 
